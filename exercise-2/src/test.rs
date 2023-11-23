bc16e87ae25a128decf53dacbf69467ba95d4677
Maxim Kosterov <maxim.kosterov@gmail.com>
2023-11-23 23:00:55 +0300

    Some cosmetics on testing

--- exercise-2/src/test.rs
+++ exercise-2/src/test.rs
@@ -1,9 +1,16 @@
-use crate::matrix::{dense::DenseRowMatrix, traits::MatrixRef};
+use crate::{
+    basic::Index,
+    matrix::{
+        dense::DenseRowMatrix,
+        traits::{MatrixFuncInitializer, MatrixRef},
+    },
+};
 
 type Scalar = f64;
 
 #[derive(Debug, Clone)]
 struct TestCase {
+    name: String,
     matrix: DenseRowMatrix<Scalar>,
     vector: Vec<Scalar>,
     answer: Vec<Scalar>,
@@ -12,7 +19,7 @@ struct TestCase {
 #[derive(Debug, Clone)]
 struct TestResult {
     answer: Vec<Scalar>,
-    norm_inf: Scalar,
+    norm_one: Scalar,
 }
 
 fn on_case(
@@ -21,33 +28,72 @@ fn on_case(
 ) -> TestResult {
     let dimension = case.matrix.dimension();
     let impl_answer = implementation(&case.matrix, &case.vector);
-    let norm_inf = (0..dimension)
+    let norm_one = (0..dimension)
         .map(|i| (impl_answer[i] - case.answer[i]).abs())
-        .fold(0.0, |m, v| if v > m { v } else { m });
+        .sum();
     TestResult {
         answer: impl_answer,
-        norm_inf,
+        norm_one,
+    }
+}
+
+fn create_fifth_case(epsilon: Scalar, dimension: Index) -> TestCase {
+    let alpha = 6.0 * epsilon;
+
+    let mut vector = vec![-1.0; dimension];
+    vector[dimension - 1] = 1.0;
+
+    let mut answer = vec![0.0; dimension];
+    answer[dimension - 1] = 1.0 / (1.0 + alpha);
+
+    let matrix = DenseRowMatrix::new_func(dimension, |i, j| {
+        if i == j {
+            1.0 + alpha
+        } else if i > j {
+            alpha
+        } else {
+            -1.0 - alpha
+        }
+    });
+
+    TestCase {
+        name: format!("Test 5 with epsilon={:e}, n={}", epsilon, dimension),
+        matrix,
+        answer,
+        vector,
     }
 }
 
 fn create_test_cases() -> Vec<TestCase> {
+    let fifth_cases = vec![1e-3, 1e-6, 1e-9, 1e-12]
+        .into_iter()
+        .map(|epsilon| {
+            vec![4, 5, 8, 16, 32]
+                .into_iter()
+                .map(move |dimension| create_fifth_case(epsilon, dimension))
+        })
+        .flatten();
     vec![
         TestCase {
+            name: String::from("Test 0"),
             matrix: DenseRowMatrix::new(3, vec![0.0, 2.0, 3.0, 1.0, 2.0, 4.0, 4.0, 5.0, 6.0]),
             answer: vec![1.0, 2.0, 3.0],
             vector: vec![13.0, 17.0, 32.0],
         },
         TestCase {
+            name: String::from("Test 1"),
             matrix: DenseRowMatrix::new(3, vec![8.0, 1.0, 1.0, 1.0, 10.0, 1.0, 1.0, 1.0, 12.0]),
             vector: vec![10.0, 12.0, 14.0],
             answer: vec![1.0, 1.0, 1.0],
         },
         TestCase {
+            name: String::from("Test 2"),
             matrix: DenseRowMatrix::new(3, vec![-8.0, 1.0, 1.0, 1.0, -10.0, 1.0, 1.0, 1.0, -12.0]),
             vector: vec![-10.0, -12.0, -14.0],
             answer: vec![375.0 / 232.0, 349.0 / 232.0, 331.0 / 232.0],
         },
         TestCase {
+            name: String::from("Test 3"),
             matrix: DenseRowMatrix::new(
                 3,
                 vec![-8.0, 9.0, 10.0, 11.0, -10.0, 7.0, 10.0, 11.0, -12.0],
@@ -56,11 +102,15 @@ fn create_test_cases() -> Vec<TestCase> {
             answer: vec![444.0 / 307.0, 358.0 / 307.0, 340.0 / 307.0],
         },
         TestCase {
+            name: String::from("Test 4"),
             matrix: DenseRowMatrix::new(3, vec![8.0, 9.0, 10.0, 11.0, 10.0, 7.0, 10.0, 11.0, 12.0]),
             vector: vec![10.0, 12.0, 14.0],
             answer: vec![16.0, -22.0, 8.0],
         },
     ]
+    .into_iter()
+    .chain(fifth_cases)
+    .collect()
 }
 
 pub fn test_cases(
@@ -68,6 +118,6 @@ pub fn test_cases(
 ) {
     for case in create_test_cases() {
         let result = on_case(&case, implementation.clone());
-        println!("Result = {:?}", result);
+        println!("name={}, norm_inf={:e}", case.name, result.norm_one);
     }
 }
