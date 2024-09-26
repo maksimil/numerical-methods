97dd829e82caa0af606ca0a3cd679ece5321e4e3
Maxim Kosterov <maxim.kosterov@gmail.com>
2024-09-26 11:32:20 +0300

    initialized exercise 7

---
+++ exercise-7/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+#define LOG_INFO(...) fprintf(stderr, __VA_ARGS__)
+
+using Scalar = double;
+using Index = std::ptrdiff_t;
+
+int main(int argc, char *argv[]) { LOG_INFO("Hellow\n"); }
